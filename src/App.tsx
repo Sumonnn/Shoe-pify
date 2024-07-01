afa155ef512a3bcdbfa5ccce9fc113bf72a6f8fb
Golam Masud <sumonkhan1081@gmail.com>
2024-07-02 02:12:09 +0530

    loader done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isTop,setIsTop] = useState(false);
+
+  useEffect(()=>{
+    moveToTop();
+  },[])
+
+  function moveToTop(){
+    setTimeout(()=>{
+        setIsTop(true);
+    },1000)
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Shoe'pify</h2>
+        <h2 className={`title ${isTop ? "top" : ""}`}>Shoe'pify</h2>
       </header>
     </div>
   );
